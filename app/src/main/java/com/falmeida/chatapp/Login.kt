0c719dc46d34d577cd7f982699ff3baa5dc7d2df
falmeida <flaviomslima@gmail.com>
2021-12-26 18:51:05 -0300

    Login and SignUp finished

--- app/src/main/java/com/falmeida/chatapp/Login.kt
+++ app/src/main/java/com/falmeida/chatapp/Login.kt
@@ -1,9 +1,12 @@
 package com.falmeida.chatapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
 
@@ -11,9 +14,45 @@ class Login : AppCompatActivity() {
     private lateinit var etPassword: EditText
     private lateinit var btnLogin: Button
     private lateinit var btnSignUp: Button
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
+        etEmail = findViewById(R.id.et_email)
+        etPassword = findViewById(R.id.et_password)
+        btnLogin = findViewById(R.id.btn_login)
+        btnSignUp = findViewById(R.id.btn_signUp)
+
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+
+        btnLogin.setOnClickListener {
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+
+            login(email, password)
+        }
+
     }
+
+    private fun login(email: String, password: String) {
+        //Login logic
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@Login, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@Login, "User does not exit", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+
 }
--- app/src/main/java/com/falmeida/chatapp/SignUp.kt
+++ app/src/main/java/com/falmeida/chatapp/SignUp.kt
@@ -1,11 +1,51 @@
 package com.falmeida.chatapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUp : AppCompatActivity() {
+
+    private lateinit var etName: EditText
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnSignUp: Button
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        etName = findViewById(R.id.et_name)
+        mAuth = FirebaseAuth.getInstance()
+        etEmail = findViewById(R.id.et_email)
+        etPassword = findViewById(R.id.et_password)
+        btnSignUp = findViewById(R.id.btn_signUp)
+
+        btnSignUp.setOnClickListener {
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+
+            signUp(email, password)
+        }
     }
+
+    private fun signUp(email: String, password: String) {
+        //User Logic
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@SignUp, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                   Toast.makeText(this@SignUp, "Something wrong", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+
 }
